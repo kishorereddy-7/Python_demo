03c5b33bb7293ff4960ec33acb42fdf4f5fac2b6
kishorereddy-7 <70094016+kishorereddy-7@users.noreply.github.com>
2021-10-25 14:19:57 +0530

    Update unit_test.py

--- unit_test.py
+++ unit_test.py
@@ -1,9 +1,4 @@
-def cuboid_volume(l):
-    return (l*l*l)
-length = [2,1.1, -2.5, 2j, 'two']
-for i in range(len(length)):
-    print ("The volume of cuboid:",cuboid_volume(length[i]))
-    
+from sp import *  
 import unittest
 
 class TestCuboid(unittest.TestCase):
